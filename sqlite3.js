df7d76f5b65ae259958763a1b1907bb0fa5121f3
Thomas Moquet <thomas.moquet@doweb.fr>
2014-07-15 00:30:32 +0200

    add YUIDoc comments

--- sqlite3.js
+++ sqlite3.js
@@ -1,55 +1,164 @@
-// my module, stored in sqlite3.js
-// patching phantomjs' require()
-var require = patchRequire(require);
-var childProcess = require("child_process");
+/**
+ * __CasperJS__ module that interfaces __SQLite 3__ databases.
+ *
+ * 		var sqlitemod = require('./sqlite3');
+ *
+ * 		sqlite = sqlitemod.sqlite3("mysqlite.db");
+ *
+ * 		sqlite.queryArray("SELECT * FROM myTable;", function(arrayResult){
+ *			// Do stuff
+ * 		}
+ *
+ * @author Thomas Moquet
+ *
+ * @module sqlite3
+ */
+ 
+ 
+var require = patchRequire(require);				// patching phantomjs' require()
+var childProcess = require("child_process");		// Requiring childProcess to execute command line
+var sqliteresultmod = require('./sqlite3result');	// Requiring SQLite3 result class
 
 
-var sqliteresultmod = require('./sqlite3result');
+/**
+ * SQLITE3_ASSOC: To return an array indexed by column name as returned in the corresponding result set
+ *
+ * @property SQLITE3_ASSOC
+ * @type String
+ * @final
+ */
+const SQLITE3_ASSOC = "SQLITE3_ASSOC";
 
 
-const SQLITE_ASSOC = "SQLITE_ASSOC";
-const SQLITE_BOTH = "SQLITE_BOTH";
-const SQLITE_NUM = "SQLITE_NUM";
+/**
+ * SQLITE3_BOTH: To return an array indexed by both column name and number as returned in the corresponding result set, starting at column 0
+ *
+ * @property SQLITE3_BOTH
+ * @type String
+ * @final
+ */
+const SQLITE3_BOTH = "SQLITE3_BOTH";
+
+
+/**
+ * SQLITE3_NUM: To return an array indexed by column number as returned in the corresponding result set, starting at column 0
+ *
+ * @property SQLITE3_NUM
+ * @type String
+ * @final
+ */
+const SQLITE3_NUM = "SQLITE3_NUM";
+
 
 exports.sqlite3 = function(db){
-	this.db = db;
+	/**
+	 * Sqlite3 database filename
+	 *
+	 * @attribute db
+	 * @type {String} sqlite3 databse filename
+	 * @required
+	 */	 
+	this._db = db;
+	
+	
+	/**
+	 * The SQL query to execute
+	 *
+	 * @private
+	 * @property _query
+	 */
 	this._query = null;
+	
+	
+	/**
+	 * The error message returned
+	 *
+	 * @private
+	 * @property _error_msg
+	 */
 	this._error_msg = null;
+	
+	
+	/**
+	 * The last insert row id
+	 *
+	 * @private
+	 * @property _lastInsertRowID
+	 */
 	this._lastInsertRowID = null;
+	
+	
+	/**
+	 * The number of database rows that were changed (or inserted or deleted) by the most recent SQL statement.
+	 *
+	 * @private
+	 * @property _changes
+	 */
 	this._changes = null;
+	
+	
+	/**
+	 * The number of database rows that were changed (or inserted or deleted) by all the SQL statements.
+	 *
+	 * @private
+	 * @property _totalChanges
+	 */
 	this._totalChanges = null;
+	
+	
+	/**
+	 * Executes a result-less query against a given database
+	 *
+	 * @method exec
+	 * @param query {String} The SQL query to execute (typically an INSERT, UPDATE, or DELETE query).
+	 * @param callback {Function} Callback the return
+	 *
+	 * @return {Boolean} Returns TRUE if the query succeeded, FALSE on failure.
+	 */
 	this.exec = function(query, callback) {
-		this._query = query.concat(";").replace(";;",";");
+		this._query = query.concat(";").replace(";;",";"); 		// Assuring having end semicolon for adding SQL query
 		var sqlite3 = this;
 		if (childProcess) {
-			childProcess.execFile("sqlite3", [this.db, this._query+"SELECT changes();SELECT total_changes();SELECT last_insert_rowid();"], null, function (err, stdout, stderr) {
-				var aStdout = stdout.split("\n");
-				aStdout.pop(); // Vire le dernier \n
-				sqlite3._lastInsertRowID = aStdout.pop();
-				sqlite3._totalChanges = aStdout.pop();
-				sqlite3._changes = aStdout.pop();
-				sqlite3._error_msg = stderr.replace("\n", "");
+			childProcess.execFile("sqlite3", [this._db, this._query+"SELECT changes();SELECT total_changes();SELECT last_insert_rowid();"], null, function (err, stdout, stderr) {
+				var aStdout = stdout.split("\n");				// Change output to array	
+				aStdout.pop(); 									// Delete last empty line
+				sqlite3._lastInsertRowID = aStdout.pop();		// Set _lastInsertRowID
+				sqlite3._totalChanges = aStdout.pop();			// Set _totalChanges
+				sqlite3._changes = aStdout.pop();				// Set _changes
+				sqlite3._error_msg = stderr.replace("\n", "");	// Delete line feed
 				if (callback && typeof(callback) === "function") {  
 					callback(stderr ? false : true);
 				}
 			});
 		}
 	};
+	
+	
+	/**
+	 * Executes an SQL query
+	 *
+	 * @method query
+	 * @param query {String} The SQL query to execute.
+	 * @param callback {Function} Callback the return
+	 *
+	 * @return {sqlite3result} Returns an {{#crossLink "sqlite3result"}}{{/crossLink}} object if the query returns results. 
+	 * Otherwise, returns TRUE if the query succeeded, FALSE on failure.
+	 */
 	this.query = function(query, callback) {
-		this._query = query.concat(";").replace(";;",";");
+		this._query = query.concat(";").replace(";;",";");		// Assuring having end semicolon for adding SQL query
 		var sqlite3 = this;
 		if (childProcess) {
-			childProcess.execFile("sqlite3", ["-header", this.db, this._query+"SELECT changes();SELECT total_changes();SELECT last_insert_rowid();"], null, function (err, stdout, stderr) {
-				var aStdout = stdout.split("\n");
-				aStdout.pop(); // Vire le dernier \n
-				sqlite3._lastInsertRowID = aStdout.pop();
-				aStdout.pop(); // Vire le label lastInsertRowID
-				sqlite3._totalChanges = aStdout.pop();
-				aStdout.pop(); // Vire le label total change
-				sqlite3._changes = aStdout.pop();
-				aStdout.pop(); // Vire le label change
-				stdout = aStdout.join("\n");
-				sqlite3._error_msg = stderr.replace("\n", "");
+			childProcess.execFile("sqlite3", ["-header", this._db, this._query+"SELECT changes();SELECT total_changes();SELECT last_insert_rowid();"], null, function (err, stdout, stderr) {
+				var aStdout = stdout.split("\n");				// Change output to array
+				aStdout.pop(); 									// Delete last empty line
+				sqlite3._lastInsertRowID = aStdout.pop();		// Set _lastInsertRowID
+				aStdout.pop(); 									// Delete last_insert_rowid column name
+				sqlite3._totalChanges = aStdout.pop();			// Set _totalChanges
+				aStdout.pop(); 									// Delete total_changes column name
+				sqlite3._changes = aStdout.pop();				// Set _changes
+				aStdout.pop();									// Delete changes column name
+				stdout = aStdout.join("\n");					// Change array to input
+				sqlite3._error_msg = stderr.replace("\n", "");	// Delete line feed
 				if (sqlite3._error_msg) {
 					sqlite3result = false;
 				} else if (!stdout){
@@ -63,26 +172,51 @@ exports.sqlite3 = function(db){
 			});
 		}
 	};
+		
+	
+	/**
+	 * Executes a query and returns a single result
+	 *
+	 * @method querySingle
+	 * @param query {String} The SQL query to execute.
+	 * @param entireRow {Boolean} By default, querySingle() returns the value of the first column returned by the query. 
+	 * If entire_row is TRUE, then it returns an array of the entire first row.
+	 * @param callback {Function} Callback the return
+	 *
+	 * @return {String} Returns the value of the first column of results
+	 * @return {Array} or an array of the entire first row (if entireRow is TRUE).
+	 * @return {Boolean} Invalid or failing queries will return FALSE.
+	 * If the query is valid but no results are returned, then NULL will be returned if entireRow is FALSE
+	 * otherwise an empty array is returned.
+	 */
 	this.querySingle = function(query, entireRow, callback) {
 		this._query = query;
 		var sqlite3 = this;
 		entireRow = entireRow ? entireRow : false;
 		if (childProcess) {
-			childProcess.execFile("sqlite3", ["-header", this.db, this._query], null, function (err, stdout, stderr) {
+			childProcess.execFile("sqlite3", ["-header", this._db, this._query], null, function (err, stdout, stderr) {
 				sqlite3._error_msg = stderr.replace("\n", "");
 				if (sqlite3._error_msg){
 					result = false;
 				} else {
 					var aStdout = stdout.split("\n");
 					var fields = aStdout.shift().split("|");
-					var row = aStdout.shift().split("|");
-					if (entireRow) {
-						var result = {};
-						for (var j=0;j<row.length; j++){
-							result[fields[j]] = row[j];
+					if (aStdout.length) {
+						var row = aStdout.shift().split("|");
+						if (entireRow) {
+							var result = {};
+							for (var j=0;j<row.length; j++){
+								result[fields[j]] = row[j];
+							}
+						} else {
+							var result = row[0];
 						}
 					} else {
-						var result = row[0];
+						if (entireRow) {
+							var result = {};
+						} else {
+							var result = null;
+						}
 					}
 				}
 				if (callback && typeof(callback) === "function") {  
@@ -91,35 +225,63 @@ exports.sqlite3 = function(db){
 			});
 		}
 	};
+			
+	
+	/**
+	 * Executes a query and fetches all rows from the result set as an array of arrays
+	 * It is similar to calling query then fetchArray for each row in the result set.
+	 *
+	 * @method queryArray
+	 * @param query {String} The SQL query to execute.
+	 *
+	 * @param [resultType=SQLITE3_BOTH] {String}
+	 *
+	 * Controls how the rows will be returned. 
+	 * This value must be one of either SQLITE3_ASSOC, SQLITE3_NUM, or SQLITE3_BOTH
+	 *
+	 * - SQLITE3_ASSOC: returns an array indexed by column name as returned in the corresponding result set
+	 * - SQLITE3_NUM: returns an array indexed by column number as returned in the corresponding result set, starting at column 0
+	 * - SQLITE3_BOTH: returns an array indexed by both column name and number as returned in the corresponding result set, starting at column 0
+	 *
+	 * @param callback {Function} Callback the return
+	 *
+	 * @return {Array} or an array of the entire first row (if entireRow is TRUE).
+	 * @return {Boolean} Invalid or failing queries will return FALSE.
+	 * If the query is valid but no results are returned, an empty array is returned.
+	 */
 	this.queryArray = function(query, resultType, callback) {
 		this._query = query;
 		var sqlite3 = this;
-		resultType = resultType ? resultType : SQLITE_BOTH;
+		resultType = resultType ? resultType : SQLITE3_BOTH;
 		if (childProcess) {
-			childProcess.execFile("sqlite3", ["-header", this.db, this._query], null, function (err, stdout, stderr) {
+			childProcess.execFile("sqlite3", ["-header", this._db, this._query], null, function (err, stdout, stderr) {
 				sqlite3._error_msg = stderr.replace("\n", "");
 				if (sqlite3._error_msg) {
 					var result = false;
 				} else {
-					var aStdout = stdout.split("\n");
-					aStdout.pop(); // Vire le dernier \n
-					var fields = aStdout.shift().split("|");
-					var resultTmp = [];
-					for (var i=0; i < aStdout.length;i++){	
-						resultTmp.push(aStdout[i].split("|"));
-					}					
-					var result = [];
-					for (var i=0; i < resultTmp.length;i++){
-						var resultLine = {};
-						for (var j=0;j<resultTmp[i].length; j++){
-							if (resultType == SQLITE_ASSOC || resultType == SQLITE_BOTH){
-								resultLine[fields[j]] = resultTmp[i][j];
+					if (stdout) {
+						var aStdout = stdout.split("\n");
+						aStdout.pop();
+						var fields = aStdout.shift().split("|");
+						var resultTmp = [];
+						for (var i=0; i < aStdout.length;i++){	
+							resultTmp.push(aStdout[i].split("|"));
+						}					
+						var result = [];
+						for (var i=0; i < resultTmp.length;i++){
+							var resultLine = {};
+							for (var j=0;j<resultTmp[i].length; j++){
+								if (resultType == SQLITE3_ASSOC || resultType == SQLITE3_BOTH){
+									resultLine[fields[j]] = resultTmp[i][j];
+								}
+								if (resultType == SQLITE3_NUM || resultType == SQLITE3_BOTH){
+									resultLine[j] = resultTmp[i][j];
+								}
 							}
-							if (resultType == SQLITE_NUM || resultType == SQLITE_BOTH){
-								resultLine[j] = resultTmp[i][j];
-							}
+							result.push(resultLine);
 						}
-						result.push(resultLine);
+					} else {
+						var result = [];
 					}
 				}
 				if (callback && typeof(callback) === "function") {  
@@ -128,18 +290,65 @@ exports.sqlite3 = function(db){
 			});
 		}
 	};
+	
+	
+	/**
+	 * Returns English text describing the most recent failed SQLite request
+	 *
+	 * @method lastErrorMsg
+	 *
+	 * @return {String} Returns an English string describing the most recent failed SQLite request.
+	 */
 	this.lastErrorMsg = function(){
 		return this._error_msg;
 	};
+	
+	
+	/**
+	 * Returns the row ID of the most recent INSERT into the database
+	 *
+	 * @method lastInsertRowID
+	 *
+	 * @return {Integer} Returns the row ID of the most recent INSERT into the database
+	 */
 	this.lastInsertRowID = function() {
 		return this._lastInsertRowID;
 	};
+	
+	
+	/**
+	 * Returns the number of database rows that were changed (or inserted or deleted) by the most recent SQL statement
+	 *
+	 * @method lastInsertRowID
+	 *
+	 * @return {Integer} Returns an integer value corresponding to the number of database rows changed (or inserted or deleted) by the most recent SQL statement.
+	 */
 	this.changes = function() {
 		return this._changes;
 	};
+	
+	
+	/**
+	 * Returns the number of database rows that were changed (or inserted or deleted) by all the SQL statements.
+	 *
+	 * @method lastInsertRowID
+	 *
+	 * @return {Integer} Returns an integer value corresponding to the number of database rows changed (or inserted or deleted) by all the SQL statement.
+	 */
 	this.totalChanges = function() {
 		return this._totalChanges;
 	};
+	
+	
+	/**
+	 * Returns the SQLite3 library version as a string constant, version date and version hash
+	 *
+	 * @method lastInsertRowID
+	 *
+	 * @param callback {Function} Callback the return
+	 *
+	 * @return {Array} Returns an associative array with the keys "versionString", "versionDate" and "versionHash".
+	 */
 	this.version = function(callback) {
 		if (childProcess) {
 			childProcess.execFile("sqlite3", ["-version"], null, function (err, stdout, stderr) {
@@ -154,5 +363,7 @@ exports.sqlite3 = function(db){
 			});
 		}
 	};
+	
+	
 	return this;
 }
--- sqlite3result.js
+++ sqlite3result.js
@@ -1,54 +1,202 @@
-// my module, stored in sqlite3result.js
-// patching phantomjs' require()
-var require = patchRequire(require);
-var childProcess = require("child_process");
+/**
+* A class that handles result sets for the SQLite 3 module.
+*
+* @class sqlite3result
+* @constructor
+*/
 
-const SQLITE_ASSOC = "SQLITE_ASSOC";
-const SQLITE_BOTH = "SQLITE_BOTH";
-const SQLITE_NUM = "SQLITE_NUM";
+
+/**
+ * SQLITE3_ASSOC: To return an array indexed by column name as returned in the corresponding result set
+ *
+ * @property SQLITE3_ASSOC
+ * @type String
+ * @final
+ */
+const SQLITE3_ASSOC = "SQLITE3_ASSOC";
+
+
+/**
+ * SQLITE3_BOTH: To return an array indexed by both column name and number as returned in the corresponding result set, starting at column 0
+ *
+ * @property SQLITE3_BOTH
+ * @type String
+ * @final
+ */
+const SQLITE3_BOTH = "SQLITE3_BOTH";
+
+
+/**
+ * SQLITE3_NUM: To return an array indexed by column number as returned in the corresponding result set, starting at column 0
+ *
+ * @property SQLITE3_NUM
+ * @type String
+ * @final
+ */
+const SQLITE3_NUM = "SQLITE3_NUM";
+
 
 exports.sqlite3result = function(stdout){
-	var aStdout = stdout.split("\n");
-	aStdout.pop(); // Vire le dernier \n
-	this.fields = aStdout.shift().split("|");
-	this.result = [];
+	/**
+	 * Sqlite3 query result output that is required for parsing result
+	 *
+	 * @attribute stdout
+	 * @type {String} sqlite3 query result output
+	 * @required
+	 */	 
+	 
+	 
+	var aStdout = stdout.split("\n"); 	// Change output to array	
+	
+	
+	/**
+	 * Setting fields name (given by -header sqlite3 option )
+	 *
+	 * @private
+	 * @property _fields
+	 */
+	this._fields = aStdout.shift().split("|");
+	
+	
+	/**
+	 * Setting raw result
+	 *
+	 * @private
+	 * @property _result
+	 */
+	this._result = [];
 	for (var i=0; i < aStdout.length;i++){	
-		this.result.push(aStdout[i].split("|"));
+		this._result.push(aStdout[i].split("|"));
 	}
-	this.numRows = this.result.length;
-	this.numColumns = this.fields.length;
+	
+	
+	/**
+	 * Setting number of returned rows
+	 *
+	 * @private
+	 * @property _numRows
+	 */
+	this._numRows = this._result.length;
+	
+	
+	/**
+	 * Setting number of returned columns
+	 *
+	 * @private
+	 * @property _numColumns
+	 */
+	this._numColumns = this._fields.length;
+	
+	
+	/**
+	 * Returns the column name
+	 *
+	 * @method columnName
+	 * @param columnNumber {Integer} Column ID
+	 * @return {String} Column name
+	 */
 	this.columnName = function(columnNumber) {
-		return this.fields[columnNumber];
+		return this._fields[columnNumber];
 	};
+
+	
+	/**
+	 * 	Fetches a result row as an associative or numerically indexed array or both
+	 *
+	 * @method fetchArray
+	 * @param [resultType=SQLITE3_BOTH] {String}
+	 *
+	 * Controls how the next row will be returned to the caller. 
+	 * This value must be one of either SQLITE3_ASSOC, SQLITE3_NUM, or SQLITE3_BOTH
+	 *
+	 * - SQLITE3_ASSOC: returns an array indexed by column name as returned in the corresponding result set
+	 * - SQLITE3_NUM: returns an array indexed by column number as returned in the corresponding result set, starting at column 0
+	 * - SQLITE3_BOTH: returns an array indexed by both column name and number as returned in the corresponding result set, starting at column 0
+	 *
+	 * @return {Array} Returns a result row as an associatively or numerically indexed array or both.
+	 * @return {Boolean} Alternately will return FALSE if there are no more rows.
+	 */
 	this.fetchArray = function(resultType) {
-		resultType = resultType ? resultType : SQLITE_BOTH;
-		var row = this.result.shift();
+		resultType = resultType ? resultType : SQLITE3_BOTH;
+		var row = this._result.shift();
 		if (!row) return false;
 		var resultLine = {};
 		for (var j=0;j<row.length; j++){
-			if (resultType == SQLITE_ASSOC || resultType == SQLITE_BOTH){
-				resultLine[this.fields[j]] = row[j];
+			if (resultType == SQLITE3_ASSOC || resultType == SQLITE3_BOTH){
+				resultLine[this._fields[j]] = row[j];
 			}
-			if (resultType == SQLITE_NUM || resultType == SQLITE_BOTH){
+			if (resultType == SQLITE3_NUM || resultType == SQLITE3_BOTH){
 				resultLine[j] = row[j];
 			}
 		}
 		return resultLine;
 	};
+	
+	
+	/**
+	 * 	Fetches all rows from a result set as an array of arrays
+	 *  It is similar to calling fetchArray for each row in the result set.
+	 *
+	 * @method fetchAll
+	 * @param [resultType=SQLITE3_BOTH] {String}
+	 *
+	 * Controls how the next row will be returned to the caller. 
+	 * This value must be one of either SQLITE3_ASSOC, SQLITE3_NUM, or SQLITE3_BOTH
+	 *
+	 * - SQLITE3_ASSOC: returns an array indexed by column name as returned in the corresponding result set
+	 * - SQLITE3_NUM: returns an array indexed by column number as returned in the corresponding result set, starting at column 0
+	 * - SQLITE3_BOTH: returns an array indexed by both column name and number as returned in the corresponding result set, starting at column 0
+	 *
+	 * @return {Array} Returns an array of the remaining rows in a result set. 
+	 * If called right after query(), it returns all rows. 
+	 * If called after fetchArray(), it returns the rest. 
+	 * If there are no rows in a result set, it returns an empty array.
+	 */
 	this.fetchAll = function(resultType) {
 		var result = [];
 		while (resultLine = this.fetchArray(resultType)){
 			result.push(resultLine);
 		}
-		if (!result.length) return false;
 		return result;
 	};
+	
+	
+	/**
+	 * Resets the result set back to the first row
+	 *
+	 * @method reset
+	 * @return {Boolean} Returns TRUE.
+	 */	
 	this.reset = function(){
-		this.result = [];
+		this._result = [];
 		for (var i=0; i < aStdout.length;i++){	
-			this.result.push(aStdout[i].split("|"));
+			this._result.push(aStdout[i].split("|"));
 		}
 		return true;	
 	};
+	
+	
+	/**
+	 * Returns the number of rows in the result set
+	 *
+	 * @method numRows
+	 * @return {Integer} Returns the number of rows in the result set
+	 */	
+	this.numRows = function(){
+		return this._numRows;
+	};
+	
+	
+	/**
+	 * Returns the number of columns in the result set
+	 *
+	 * @method numColumns
+	 * @return {Integer} Returns the number of columns in the result set
+	 */	
+	this.numColumns = function(){
+		return this._numColumns;
+	};
+	
+	
 	return this;
 };
